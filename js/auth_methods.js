2c995e60a9f940b8f78d3214df7d9e8a30784177
gurujilove108 <dylan.dannenhauer@gmail.com>
2016-03-17 15:23:54 -0400

    made bug fix

--- js/auth_methods.js
+++ js/auth_methods.js
@@ -2,8 +2,8 @@
 function googleAuth() {
 	if (document.URL.indexOf("localhost") != -1) 
 		window.location = "http://localhost:" + window.location.port + "/authenticate_google";
-	else if (document.URL.indexOf("dtd3dgames") != -1) {
-		window.location = "https://dtd3dgames.appspot.com/authenticate_google";
+	else if (document.URL.indexOf(".appspot") != -1) {
+		window.location = "https://dylans-gae-template.appspot.com/authenticate_google";
 	}
 }
 
